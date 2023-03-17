97698ce824915d5b458eaaf960fd9f5d29593c39
Tomi Lui <64671485+6momo1@users.noreply.github.com>
2023-03-16 22:07:59 -0700

    basic form setup created

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,18 @@
-<h1 class="text-3xl font-bold underline">
-    Hello world!
-  </h1>
- 
-  
-  
-  <style lang="postcss">
+<script>
+    import TodoForm from "../components/TodoForm.svelte";
+</script>
+
+<main>
+    <div class="m-10">
+        <h1 class="text-2xl font-bold text-center text-gray-800 md:text-3xl">
+            My Todos
+        </h1>
+        <TodoForm />
+    </div>
+</main>
+
+<style lang="postcss">
     :global(html) {
-      background-color: theme(colors.gray.100);
+        background-color: theme(colors.gray.100);
     }
-  </style>
+</style>
