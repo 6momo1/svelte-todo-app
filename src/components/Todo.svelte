3ce54da4bb73d86ea06b904eb8a299c097eab488
Tomi Lui <64671485+6momo1@users.noreply.github.com>
2023-03-17 22:22:10 -0700

    cleanup

--- src/components/Todo.svelte
+++ src/components/Todo.svelte
@@ -31,11 +31,4 @@
     >
         Delete
     </button>
-    <button
-        type="button"
-        class="duration-200 transition-all box-border bg-blue-400 bg-opacity-20 text-primary-400 hover:bg-opacity-30 buttonSize--large"
-        on:click={() => deleteTodo(todo.id)}
-    >
-        Delete
-    </button>
 </li>
